3eb20b786057271cf9b9ad655c53f38968c1b689
table <1017446883@qq.com>
2022-01-12 10:42:23 +0800

    2022年01月12日 10:42:23作业

--- lodash/tablezhuozhuo-lodash.js
+++ lodash/tablezhuozhuo-lodash.js
@@ -1,7 +1,7 @@
 var tablezhuozhuo = {
   chunk: function (array, size) {
-    var size = size || 1;
-    var result = [];
+    var size = size || 1
+    var result = []
     var l = arr.length; //数组的长度
     var s = Math.ceil(l / size)
     for (var i = 0; i < s; i++) {
