f89422414b099c56893ea72a59836df6e5f256f7
table <1017446883@qq.com>
2022-01-13 10:39:07 +0800

    2022年01月13日 10:39:07作业

--- lodash/tablezhuozhuo-lodash.js
+++ lodash/tablezhuozhuo-lodash.js
@@ -9,5 +9,50 @@ var tablezhuozhuo = {
     }
     return result
   },
-  compact: function () { },
+  compact: function (array) {
+    var result = []
+    for (var i = 0; i < array.length; i++) {
+      if (array[i]) {
+        result.push(array[i])
+      }
+    }
+    return result
+  },
+  difference: function (array, [values]) {
+    var result = []
+    for (var i = 0; i < array.length; i++) {
+      for (var j = 0; j < [values].length; j++) {
+        if (array[i] == [values][j]) {
+          break
+        }
+        result.push(array[i])
+      }
+    }
+    return result
+  },
+  differenceBy: function (array, [values]) {
+
+  },
+  differenceWith: function (array, [values]) {
+
+  },
+  drop: function (array, n) {
+    if (n == undefined) {
+      n = 1
+    }
+    l = array.length
+    if (l < n) {
+      array = []
+      return array
+    }
+    for (var i = 1; i < l; i++) {
+      if (i <= n) {
+        array.shift()
+      }
+    }
+    return array
+  },
+  dropRight: function (array, [n = 1]) {
+
+  },
 }
