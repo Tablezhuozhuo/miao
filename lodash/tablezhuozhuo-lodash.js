4af1f7550e68e97f955cd8216a18fa645e00ebda
table <1017446883@qq.com>
2022-01-13 13:34:49 +0800

    2022年01月13日 13:34:49作业

--- lodash/tablezhuozhuo-lodash.js
+++ lodash/tablezhuozhuo-lodash.js
@@ -82,7 +82,7 @@ var tablezhuozhuo = {
 
   },
   flatten: function (array) {
-     result = []
+    result = []
     for (var i = 0; i < array.length; i++) {
       l = array[i].length
       if (l) {
@@ -95,19 +95,6 @@ var tablezhuozhuo = {
     return result
   },
   flattenDeep: function (array) {
-    result = []
-    if () {
 
-    }
-    for (var i = 0; i < array.length; i++) {
-      l = array[i].length
-      if (l) {
-        for (var j = 0; j < l; j++) {
-          result.push(array[i][j])
-        }
-      }
-      else { result.push(array[i]) }
-    }
-    return flattenDeep(result)
   },
 }
